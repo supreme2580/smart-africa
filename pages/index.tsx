fba09ba807812d098f68d2e7ae560c95960643ea
Victor Omorogbe <victoromorogbe69@gmail.com>
2022-08-01 23:38:16 +0100

    suprememods

--- pages/index.tsx
+++ pages/index.tsx
@@ -36,7 +36,7 @@ export default Home
 export const getServerSideProps: GetServerSideProps = async (context) => {
 
   context.res.setHeader(
-    'Cache-Control: no-caches', 'public, s-maxage=0, stale-while-revalidate=0'
+    'Cache-Control', 'public, s-maxage=0, stale-while-revalidate=0'
   )
 
   const segment = context.query.segment?.toString().toLocaleLowerCase() || "english language"
